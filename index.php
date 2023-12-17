519a17447ccd4b4030ca349d95857b78a5e20e19
wfarrugia <98620534+wfarrugia@users.noreply.github.com>
2023-12-17 20:39:08 +0000

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<?php
+include '\xampp\htdocs\Registration\private\autoload.php';
+
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link type="text/css" rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- for mobile responsiveness-->
+    
+    <link type="text/css" rel="stylesheet" href="style.css">
+    <title>signup</title>
+</head>
+
+<body>
+    <div class="NavBar">
+        <div class="logo">
+            <img src="logo.png" alt="Logo">
+        </div>
+        <div class="search">
+            <input type="text" placeholder="Search">
+            <button type="submit">Search</button>
+        </div>
+        <div class="profile">
+            <a href="profile.php">Profile</a>
+        </div>
+
+        <div class="logout">
+            <a href="logout.php">Logout</a>
+
+            
+    </div>
+
+</div>
+</body>
+</html>
+
+
+
+            
